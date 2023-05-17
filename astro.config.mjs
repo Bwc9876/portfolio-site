1a95583e2b82d459573027514c76f5bb7c377b8f
Ben C <bwc9876@gmail.com>
2023-05-17 14:21:05 -0400

    Remove react dependencies

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,8 +3,6 @@ import mdx from "@astrojs/mdx";
 import image from "@astrojs/image";
 import sitemap from "@astrojs/sitemap";
 
-import react from "@astrojs/react";
-
 // https://astro.build/config
 export default defineConfig({
     site: "https://bwc9876.dev",

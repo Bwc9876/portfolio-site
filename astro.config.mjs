453f48eee2a920c927e97e8d5ab9564105fda580
Ben C <bwc9876@gmail.com>
2023-05-18 21:29:56 -0400

    Fix Footer, Update Deps

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,6 +6,7 @@ import sitemap from "@astrojs/sitemap";
 // https://astro.build/config
 export default defineConfig({
     site: "https://bwc9876.dev",
+    compressHTML: true,
     integrations: [
         mdx(),
         image({

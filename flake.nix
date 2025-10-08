97b96ac51f5ff5371c13e10b9ee6947c88412209
Ben C <bwc9876@gmail.com>
2025-10-08 09:43:07 -0400

    Update Deps, Add ASL

--- flake.nix
+++ flake.nix
@@ -1,27 +1,33 @@
 {
   inputs.flakelight.url = "github:nix-community/flakelight";
   outputs = {flakelight, ...}:
-    flakelight ./. ({lib, ...}: {
-      systems = lib.systems.flakeExposed;
-      package = pkgs: let
-        src = ./.;
-      in
-        pkgs.buildNpmPackage {
-          name = "portfolio-site";
-          version = "0.0.0";
-          inherit src;
-          packageJSON = ./package.json;
-          npmDeps = pkgs.importNpmLock {
-            npmRoot = src;
+    flakelight ./. (
+      {lib, ...}: {
+        systems = lib.systems.flakeExposed;
+        package = pkgs: let
+          src = ./.;
+        in
+          pkgs.buildNpmPackage {
+            name = "portfolio-site";
+            version = "0.0.0";
+            inherit src;
+            packageJSON = ./package.json;
+            npmDeps = pkgs.importNpmLock {
+              npmRoot = src;
+            };
+            npmConfigHook = pkgs.importNpmLock.npmConfigHook;
+            installPhase = "cp -r dist/ $out";
+            ASTRO_TELEMETRY_DISABLED = 1;
           };
-          npmConfigHook = pkgs.importNpmLock.npmConfigHook;
-          installPhase = "cp -r dist/ $out";
-          ASTRO_TELEMETRY_DISABLED = 1;
+        devShell.packages = pkgs:
+          with pkgs; [
+            nodejs_24
+          ];
+        formatters = pkgs: let
+          alejandra = "${pkgs.alejandra}/bin/alejandra .";
+        in {
+          "*.nix" = alejandra;
         };
-      devShell.packages = pkgs: with pkgs; [nodejs_23 alejandra nodePackages.prettier];
-      formatters = {
-        "*.nix" = "alejandra .";
-        "*.{js,ts,astro,json,yml,mdx}" = "prettier --write .";
-      };
-    });
+      }
+    );
 }

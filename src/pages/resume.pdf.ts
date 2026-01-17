eed5518c152164155c5a06080895d9f65f28ba7f
Ben C <bwc9876@gmail.com>
2026-01-17 12:05:26 -0500

    More Blog Post yay!

--- src/pages/resume.pdf.ts
+++ src/pages/resume.pdf.ts
@@ -1,15 +1,15 @@
-import rawTypst from "@assets/resume.typ?raw"; 
+import rawTypst from "@assets/resume.typ?raw";
 import { spawn } from "node:child_process";
 
 const compileTypst = (raw: string): Promise<Buffer> => {
-  const cmd = spawn("typst", ["compile", "-f", "pdf", "-", "-"], {stdio: "pipe"});
+  const cmd = spawn("typst", ["compile", "-f", "pdf", "-", "-"], { stdio: "pipe" });
 
   cmd.stdin.write(raw);
   cmd.stdin.end();
 
   return new Promise((resolve, reject) => {
     cmd.on("error", reject);
-    
+
     let stderr = "";
     cmd.stderr.setEncoding("utf8");
     cmd.stderr.on("data", (data) => {
@@ -31,16 +31,16 @@ const compileTypst = (raw: string): Promise<Buffer> => {
           resolve(stdout as Buffer);
         } else {
           reject(new Error(`No stdout, Stderr: ${stderr}`));
-        } 
+        }
       } else {
         reject(new Error(`Exited with code ${code} Stderr: ${stderr}`));
-      } 
+      }
     });
   });
 };
 
 export async function GET() {
   const data = await compileTypst(rawTypst);
-  // @ts-expect-error "idk why it thinks this is wrong? Astro supports responses from a Buffer" 
+  // @ts-expect-error "idk why it thinks this is wrong? Astro supports responses from a Buffer"
   return new Response(data);
 }
